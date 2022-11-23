1d10710fb2ebb71be777f3f853ee84609c2c8ad0
Hunter <realisvenator@gmail.com>
2022-11-23 12:57:28 -0600

    added basic seeding

---
+++ server/seeds/employees.js
@@ -0,0 +1,41 @@
+const emps = [
+    {
+        username: "user1",
+        password: "password",
+        salary: 90000,
+        title: "Lead"
+    },
+    {
+        username: "user2",
+        password: "password",
+        salary: 85000,
+        title: "Head Aritech"
+    },
+    {
+        username: "user3",
+        password: "password",
+        salary: 120000,
+        title: "CFO"
+    },
+    {
+        username: "user4",
+        password: "password",
+        salary: 65000,
+        title: "CIO"
+    },
+    {
+        username: "user5",
+        password: "password",
+        salary: 2000,
+        title: "SEO"
+    },
+    {
+        username: "user6",
+        password: "password",
+        salary: 155000,
+        title: "ALP"
+    },
+]
+
+
+module.exports = emps;
---
+++ server/seeds/projects.js
@@ -0,0 +1,20 @@
+const proj = [
+    {
+        name: "Project1",
+        description: "This is describing 1",
+        works: [],
+        image: "placeholder.jpg",
+        link: "www.youtube.com",
+        completed: true,
+    },
+    {
+        name: "Projec2",
+        description: "this is describing 2",
+        works: [],
+        image: "placeholder.jpg",
+        link: "www.youtube.com",
+        completed: true,
+    },
+]
+
+module.exports = proj;
