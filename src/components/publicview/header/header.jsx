1928ce454300a91642fec9a0b5eabc55299c15c2
AaronVenema <34764586+AaronVenema@users.noreply.github.com>
2022-11-23 13:27:43 -0600

    navigation of pages

--- src/components/publicview/header/header.jsx
+++ src/components/publicview/header/header.jsx
@@ -1,21 +1,21 @@
 import React from "react"
 import {Container, Navbar, Nav} from 'react-bootstrap';
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 
 function Header (){
     return(
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container className="text-center">
-        <Navbar.Brand > Home</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/"> Home</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link >MachDev Projects</Nav.Link>
-              <Nav.Link >MachDev Departments</Nav.Link>
-              <Nav.Link >MachDev Service Request</Nav.Link>
-              <Nav.Link >MachDev Employee Login</Nav.Link>
-              <Nav.Link >Current Projects</Nav.Link>
+            <Nav.Link as={Link} to="/departmentView">MachDev Departments</Nav.Link>
+            <Nav.Link as={Link} to="/employeeView">MachDev Employees</Nav.Link>
+            <Nav.Link as={Link} to="/completedProjects" >MachDev Projects</Nav.Link>
+            <Nav.Link as={Link} to="/companyInfo">MachDev Info</Nav.Link>
+            <Nav.Link as={Link} to="/serviceRequest">MachDev Service Request</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
