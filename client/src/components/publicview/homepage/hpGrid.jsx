aa0f732822bbd2e43505df1f847e86cca67c4896
Mimimoha <mohamedm1987@gmail.com>
2022-11-29 13:13:32 -0600

    styled homepage

--- client/src/components/publicview/homepage/hpGrid.jsx
+++ client/src/components/publicview/homepage/hpGrid.jsx
@@ -5,10 +5,10 @@ const HpGrid = () => (
   <Grid className='justify-content-center'>
    
     
-      <Grid.Column width={6}>
-        <Segment as='h3' textAlign='center' >Welcome to MACHDEV, where you can find solutions for all your tech needs. </Segment>
+      <Grid.Column >
+        <Segment as='h3'  className= 'bg-secondary justfiy-content-center text-white'textAlign='center' >Welcome to MACHDEV, where you can find solutions for all your tech needs. </Segment>
         <Image src="/images/machdev-logo.png"/>
-        <Segment as='h3' textAlign='center'>We have the knowledge and experience to take care of clients, from Front-End, Network, Servers, Web Hosting, and Cloud solutions. With our wide variety of services; this allows us to give you the total solution.</Segment>
+        <Segment as='h3'  className= 'bg-secondary justfiy-content-center text-white'textAlign='center'>We have the knowledge and experience to take care of clients, from Front-End, Network, Servers, Web Hosting, and Cloud solutions. With our wide variety of services; this allows us to give you the total solution.</Segment>
       </Grid.Column>
    
 
