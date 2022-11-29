86416ebb1bfd9c6285980e31cacc56f955e3b019
Hunter <realisvenator@gmail.com>
2022-11-29 12:07:51 -0600

    fetch for departments

--- client/src/components/publicview/departmentviewer/departmentview.jsx
+++ client/src/components/publicview/departmentviewer/departmentview.jsx
@@ -1,13 +1,23 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import DvGrid from "./dvGrid"
 import { Container } from "semantic-ui-react"
 import Header from "../header/header"
 import Footer from "../footer/footer"
 function DepartmentView() {
+  const [data, setData] = useState(null);
+
+  useEffect(()=>{
+    fetch("/api/department")
+    .then(response=> response.json())
+    .then(datas => setData(datas.payload));
+  },[]);
   return (
     <Container>
       <Header />
-      <DvGrid />
+      {data && data.map((single)=>
+          <DvGrid single={single} key={single._id} />
+        )}
+      {/* <DvGrid /> */}
       <Footer />
     </Container>
      
