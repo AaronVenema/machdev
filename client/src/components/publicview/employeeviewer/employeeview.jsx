6bd8aab63ed5eb8499db39c1468d4db0190c0242
Hunter <realisvenator@gmail.com>
2022-11-29 11:59:47 -0600

    title tweaks

--- client/src/components/publicview/employeeviewer/employeeview.jsx
+++ client/src/components/publicview/employeeviewer/employeeview.jsx
@@ -3,21 +3,40 @@ import EvGrid from "./evGrid"
 import { Container } from "semantic-ui-react"
 import Header from "../header/header"
 import Footer from "../footer/footer"
+
+// const empDef = {
+//   "Lead": "lead-logo.png",
+//   "Head Architect": "head-architect-logo.pnmg",
+//   "CFO": "cfo-logo.png",
+//   "CIO": "cio",
+//   "SEO": "seo-logo.png",
+//   "ALP": "alp-logo.png"
+
+// }
+
+
+
 function EmployeeView() {
   const [data, setData] = useState(null);
-
+  const empDef = {
+    "Lead": "lead-logo.png",
+    "Head Architect": "head-architect-logo.pnmg",
+    "CFO": "cfo-logo.png",
+    "CIO": "cio",
+    "SEO": "seo-logo.png",
+    "ALP": "alp-logo.png"
+  
+  }
   useEffect(()=>{
-    fetch("/api/department")
+    fetch("/api/employee")
     .then(response=> response.json())
     .then(datas => setData(datas.payload));
   },[]);
-  
   return (
     <Container>
       <Header />
-        {console.log(data)}
         {data && data.map((single)=>
-          <EvGrid single={single} key={single.name} />
+          <EvGrid single={{...single,pic: empDef[single.title]}} key={single._id} />
         )}
       <Footer />
     </Container>
--- client/src/components/publicview/employeeviewer/evGrid.jsx
+++ client/src/components/publicview/employeeviewer/evGrid.jsx
@@ -6,8 +6,8 @@ const EvGrid = ({single}) => {
     <Grid columns={3} divided>
       <Grid.Row>
         <Grid.Column>
-          <Image src="https://via.placeholder.com/150" />
-          <Segment>{single.name}</Segment>
+          <Image src={`images/${single.pic}`} />
+          <Segment>{single.firstName}</Segment>
         </Grid.Column>
       </Grid.Row>
     </Grid>
