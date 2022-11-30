a406caedea68f3e805a18ec327b96ab46f7faa8e
Hunter <realisvenator@gmail.com>
2022-11-30 12:44:15 -0600

    we do routing

--- client/src/App.js
+++ client/src/App.js
@@ -6,38 +6,17 @@ import {
   Navigate
 } from "react-router-dom";
 import './App.css';
-import EmployeeView from "./components/publicview/employeeviewer/employeeview";
-import DepartmentView from "./components/publicview/departmentviewer/departmentview";
-import CompletedProjects from './components/publicview/completedprojects/completedprojects';
-import Homepage from "./components/publicview/homepage/homepage";
-import ServiceRequest from "./components/publicview/servicerequest/servicerequest"
-import EmployeeLogin from "./components/publicview/employeelogin/employeelogin"
 import { AppProvider } from "./components/publicview/utils/AppContext"
 
-import CurrentProjects from "./components/internalview/currentprojects/currentprojects"
-import EmployeeLogout from "./components/internalview/employeelogout/employeelogout"
-import IHomepage from "./components/internalview/ihomepage/iHomepage";
-import PmBoard from "./components/internalview/projectsmessageboard/pmBoard";
+import Nav from "./components/Nav"
+
 function App() {
   return (
-    <AppProvider value={{}}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/employeeView" element={<EmployeeView />} />
-          <Route path="/departmentView" element={<DepartmentView />} />
-          <Route path="/completedProjects" element={<CompletedProjects />} />
-          <Route path="/serviceRequest" element={<ServiceRequest />} />
-          <Route path="/employeeLogin" element={<EmployeeLogin />} />
-          {/* internal view */}
-          <Route path="/currentProjects" element={<CurrentProjects />} />
-          <Route path="/employeeLogout" element={<EmployeeLogout />} />
-          <Route path="/iHomepage" element={<IHomepage />} />
-          <Route path="/pmBoard" element={<PmBoard />} />
-          <Route path="*" element={<Navigate to="/"/>} />
-        </Routes>
-      </Router>
-    </AppProvider>  
+    <Router>
+      <AppProvider>
+        <Nav/>
+      </AppProvider>  
+    </Router>
   );
 }
 
---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {Routes, Route, Navigate} from "react-router-dom";
+import { useAppContext } from "./publicview/utils/AppContext";
+
+import CurrentProjects from "./internalview/currentprojects/currentprojects"
+import EmployeeLogout from "./internalview/employeelogout/employeelogout"
+import IHomepage from "./internalview/ihomepage/iHomepage";
+import PmBoard from "./internalview/projectsmessageboard/pmBoard";
+import EmployeeView from "./publicview/employeeviewer/employeeview";
+import DepartmentView from "./publicview/departmentviewer/departmentview";
+import CompletedProjects from './publicview/completedprojects/completedprojects';
+import Homepage from "./publicview/homepage/homepage";
+import ServiceRequest from "./publicview/servicerequest/servicerequest"
+import EmployeeLogin from "./publicview/employeelogin/employeelogin"
+
+
+const Nav = () =>{
+    const { appState } = useAppContext();
+    return (
+        <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/employeeView" element={<EmployeeView />} />
+            <Route path="/departmentView" element={<DepartmentView />} />
+            <Route path="/completedProjects" element={<CompletedProjects />} />
+            <Route path="/serviceRequest" element={<ServiceRequest />} />
+            <Route path="/employeeLogin" element={<EmployeeLogin />} />
+            {appState.employee &&
+                <>
+                    <Route path="/currentProjects" element={<CurrentProjects />} />
+                    <Route path="/employeeLogout" element={<EmployeeLogout />} />
+                    <Route path="/iHomepage" element={<IHomepage />} />
+                    <Route path="/pmBoard" element={<PmBoard />} />
+                </>
+
+            }
+            <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+    );
+}
+
+
+export default Nav;
