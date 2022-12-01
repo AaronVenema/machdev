50627e3a4506f0d1c13cbcbbfeb0f650eb149dd8
fortu038 <fortu038@umn.edu>
2022-12-01 11:59:40 -0600

    added code to to iheader to make logout a click rather than a page

--- client/src/components/internalview/header/iheader.jsx
+++ client/src/components/internalview/header/iheader.jsx
@@ -2,23 +2,44 @@ import React from "react"
 import {Container, Navbar, Nav} from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
+import { useAppContext } from "../../publicview/utils/AppContext";
+import Cookie from "js-cookie";
 
 function IHeader (){
-    return(
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container className="text-center">
-        <Navbar.Brand as={Link} to="/iHomepage"> Home</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-            <Nav.Link as={Link} to="/currentProjects" >Projects</Nav.Link>
-            <Nav.Link as={Link} to="/iServiceRequest">Incoming Requests</Nav.Link>
-            <Nav.Link as={Link} to="/employeeLogout">Employee Logout</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    )
+  const { appState, setAppState } = useAppContext();
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    const lookupCheck = await fetch("/api/employee/lookup", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" }
+    })
+    const lookupResult = await lookupCheck.json();
+    console.log(lookupResult);
+
+    // If the login was good, save the returned token as a cookie
+    if( lookupResult.result === "success" ){
+      Cookie.remove("auth-token", lookupResult.token)
+      setAppState({...appState, employee: lookupResult.employee})
+    }
+  }
+
+  return(
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container className="text-center">
+      <Navbar.Brand as={Link} to="/iHomepage"> Home</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+          <Nav.Link as={Link} to="/currentProjects" >Projects</Nav.Link>
+          <Nav.Link as={Link} to="/iServiceRequest">Incoming Requests</Nav.Link>
+          <Nav.Link as={Link} to="/employeeLogin" onClick={handleLogout}>Employee Logout</Nav.Link>
+          {/* ^ Even though the above is a valid extension its getting pushed to the catch all */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
 }
 export default IHeader
 
