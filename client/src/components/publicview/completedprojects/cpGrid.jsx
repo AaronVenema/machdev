0901ab628239193c61c59503a346efe48a2e208c
AaronVenema <34764586+AaronVenema@users.noreply.github.com>
2022-12-01 10:23:58 -0600

    ha gotem!

--- client/src/components/publicview/completedprojects/cpGrid.jsx
+++ client/src/components/publicview/completedprojects/cpGrid.jsx
@@ -5,7 +5,7 @@ const CpGrid = ({ single }) => {
   return (
     <Grid columns={3} divided className='justify-content-evenly'>
     <Grid.Column className="mt-4 justify-content-evenly">
-    <Image src={`images/${single.image}`} />
+    <Image href="https://www.youtube.com/watch?v=eBGIQ7ZuuiU" target="_blank" src={`images/${single.image}`} alt="ha gotem!" />
     </Grid.Column>
     <Grid.Column className="mt-4 justify-content-evenly">
       <Segment className='d-flex m-4  text-center justify-content-evenly bg-secondary text-white' style={{ fontSize: ".8vw" }} > {single.name} </Segment>
